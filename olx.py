3d497ec8483061c09691b3addc60e8b8a6da085b
eughrat <michal.piernicki@gmail.com>
2022-08-24 15:00:50 +0200

    yfg

--- olx.py
+++ olx.py
@@ -3,6 +3,10 @@ from requests import get
 
 URL = 'https://www.olx.pl/d/nieruchomosci/mieszkania/sprzedaz/gdansk/'
 
+def parse_price(price):
+    parsed_price = price.replace(' ','').replace('zł', '').replace(',','.')
+    return float(parsed_price)
+
 page = get(URL)
 bs = BeautifulSoup(page.content, 'html.parser')
 
@@ -10,7 +14,7 @@ for offer in bs.find_all('div', class_='css-19ucd76'):
     try:
         location = offer.find('p', class_='css-p6wsjo-Text eu5v0x0').get_text().strip().split('-')[0]
         title = offer.find('h6', class_='css-v3vynn-Text eu5v0x0').get_text().strip()
-        price = offer.find('p', class_='css-wpfvmn-Text eu5v0x0').get_text().strip()
+        price = parse_price(offer.find('p', class_='css-wpfvmn-Text eu5v0x0').get_text().strip())
     except:
         continue
 
